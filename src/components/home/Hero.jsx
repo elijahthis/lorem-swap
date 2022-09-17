c1a11b7078d4791fb4408345ab6923b344df0a1a
elijahthis <emperorelijah007@gmail.com>
2022-09-17 23:01:37 +0100

    image interactions

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,20 +1,36 @@
 import "./styles.scss";
 import { TbCurrencyNaira } from "react-icons/tb";
 import { BsArrowRight } from "react-icons/bs";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const Hero = () => (
-	<div className="Hero">
-		<div className="main-text">
-			<h1>Lorem Swap</h1>
-			<p>Withdraw crypto directly to your bank account</p>
-		</div>
-		<div className="side-show">
-			<div className="txt">
-				<p>Crypto</p> <BsArrowRight /> <p>₦aira.</p>
+const Hero = () => {
+	const [current, setCurrent] = useState(0);
+	const currencies = ["eth", "btc", "bnb", "ada", "sol", "avax", "fil", "usdt"];
+
+	const changeInterval = setInterval(() => {
+		setCurrent((current + 1) % currencies.length);
+	}, 3000);
+
+	return (
+		<div className="Hero">
+			<div className="main-text">
+				<h1>Lorem Swap</h1>
+				<p>Withdraw crypto directly to your bank account</p>
 			</div>
-			<div className="robo-robo"></div>
+			<div className="side-show">
+				<div className="txt">
+					{/* <p>Crypto</p> */}
+					<img
+						src={`https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/${currencies[current]}.png`}
+						alt=""
+					/>
+					<BsArrowRight /> <p>₦aira.</p>
+				</div>
+				<div className="robo-robo"></div>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Hero;
