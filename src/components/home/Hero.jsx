49e6cfec406e26fb4e4f907791e2904d040a0b25
elijahthis <emperorelijah007@gmail.com>
2022-09-17 21:56:07 +0100

    more landing page tweaks + netlify redirects

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,12 +1,20 @@
 import "./styles.scss";
+import { TbCurrencyNaira } from "react-icons/tb";
+import { BsArrowRight } from "react-icons/bs";
 
 const Hero = () => (
-    <div className="Hero">
-        <div className="main-text">
-            <h1>Lorem Swap</h1>
-            <p>Withdraw crypto directly to your bank account</p>
-        </div>
-    </div>
+	<div className="Hero">
+		<div className="main-text">
+			<h1>Lorem Swap</h1>
+			<p>Withdraw crypto directly to your bank account</p>
+		</div>
+		<div className="side-show">
+			<div className="txt">
+				<p>Crypto</p> <BsArrowRight /> <p>₦aira.</p>
+			</div>
+			<div className="robo-robo"></div>
+		</div>
+	</div>
 );
 
 export default Hero;
